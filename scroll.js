b58cdcd603067f5cc5566183070bed49afb81190
AbhijitBenny <abhijitbenny@gmail.com>
2024-11-17 11:39:11 +0530

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,36 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".show-once", {
+  reset: false
+});
+
+ScrollReveal().reveal(".fade-in", {
+  duration: 5000,
+  move: 0
+});
+ScrollReveal().reveal(".scaleUp", {
+    duration: 4000,
+    scale: 0.85
+  });
+
+  ScrollReveal().reveal(".fade-in", {
+    duration: 5000,
+    move: 0
+  });
+
+  ScrollReveal().reveal(".fade-in4", {
+    duration: 6000,
+    move: 0
+  });
+  ScrollReveal().reveal(".fade-in3", {
+    duration: 7000,
+    move: 0
+  });
+  ScrollReveal().reveal(".fade-in2", {
+    duration: 8000,
+    move: 0
+  });
+  ScrollReveal().reveal(".fade-in1", {
+    duration: 9000,
+    move: 0
+  });
